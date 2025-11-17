bf05af0e6176fd7461b983f5c1cca5958f886050
dominhduc03022004 <dominhduc03022004@gmail.com>
2025-11-17 18:35:52 +0700

    add model Task, User

---
+++ backend/src/model/Task.js
@@ -0,0 +1,33 @@
+import mongoose, { Schema } from "mongoose";
+
+const taskSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    status: {
+      type: String,
+      enum: ["active", "complete"],
+      default: "active",
+    },
+    completedAt: {
+      type: Date,
+      default: null,
+    },
+    userId: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+        required: true,
+      },
+    ],
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Task = mongoose.model("Task", taskSchema);
+export default Task;
---
+++ backend/src/model/User.js
@@ -0,0 +1,57 @@
+import mongoose, { Schema } from "mongoose";
+
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      trim: true,
+      unique: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      trim: true,
+      unique: true,
+    },
+    hashedPassword: {
+      type: String,
+      required: true,
+    },
+    streak: {
+      type: Number,
+      default: 0,
+    },
+    fullName: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    avaterUrl: {
+      type: String,
+    },
+    avaterId: {
+      type: String,
+    },
+    bio: {
+      type: String,
+      maxlength: 500,
+    },
+    phone: {
+      type: String,
+      sparse: true,
+    },
+    tasks: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Task",
+      },
+    ],
+  },
+  {
+    timestamps: true,
+    versionKey: false,
+  }
+);
+
+export default mongoose.model("User", userSchema);
